47ebc2094a1f3cd3a73dacdaff99ddd322d4c36d
thsdud135 <kytbible@gmail.com>
2023-11-13 18:08:13 +0900

    feat: 메인 페이지 UI
    메인 페이지 지도 연결, 버튼 생성
    다이얼로그 페이지
    local properties 설정
    
    Closes: #4

--- Android/FunBox/app/build.gradle.kts
+++ Android/FunBox/app/build.gradle.kts
@@ -1,20 +1,27 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
+
 android {
     namespace = "com.example.funbox"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.funbox"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "MAP_API_KEY", getApiKey("MAP_API_KEY"))
+        manifestPlaceholders["MAP_API_KEY"] = getApiKey("MAP_API_KEY")
     }
 
     buildTypes {
@@ -33,6 +40,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+fun getApiKey(key: String): String {
+    return gradleLocalProperties(rootDir).getProperty(key)
 }
 
 dependencies {
@@ -41,6 +56,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("com.naver.maps:map-sdk:3.17.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
