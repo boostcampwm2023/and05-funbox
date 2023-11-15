45cfccaf99e81d6e6e17d4ce13a24f9e5fc14ac8
littlesam95 <littlesam95@naver.com>
2023-11-15 11:29:31 +0900

    [chore] build.gradle 및 Conflict 수정
    dev_and를 git pull한 후 build.gradle에서의 오류 및 MapFragment, MapViewModel에서의 Conflict가 발생해 이것을 수정

--- Android/FunBox/app/build.gradle.kts
+++ Android/FunBox/app/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import java.util.Properties
 
 plugins {
     id("com.android.application")
@@ -15,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.example.funbox"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -43,6 +42,7 @@ android {
     }
 
     buildFeatures {
+        //noinspection DataBindingWithoutKapt
         dataBinding = true
         viewBinding = true
         buildConfig = true
@@ -55,26 +55,26 @@ fun getApiKey(key: String): String {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("androidx.activity:activity-ktx:1.7.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
     implementation("com.naver.maps:map-sdk:3.17.0")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
     implementation("io.coil-kt:coil:2.0.0-rc03")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.8.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.6.0")
     
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation ("com.squareup.retrofit2:retrofit:2.6.4")
-    implementation ("com.squareup.retrofit2:retrofit-gson:2.6.4")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
--- Android/FunBox/app/src/main/java/com/example/funbox/presentation/map/MapFragment.kt
+++ Android/FunBox/app/src/main/java/com/example/funbox/presentation/map/MapFragment.kt
@@ -3,8 +3,6 @@ package com.example.funbox.presentation.map
 import com.example.funbox.presentation.BaseFragment
 import com.example.funbox.MessageDialog
 import com.example.funbox.R
-
-<<<<<<< Updated upstream
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -31,36 +29,12 @@ class MapFragment : Fragment() {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.mapViewModel=viewModel
-=======
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import com.example.funbox.databinding.FragmentProfileBinding
-
-
-class MapFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_map) {
-
-    private val viewModel: MapViewModel by viewModels()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
->>>>>>> Stashed changes
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-<<<<<<< Updated upstream
         binding.floatingActionButton.setOnClickListener{
             toggleFab()
         }
@@ -89,8 +63,3 @@ class MapFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_map) 
 
 
 }
-=======
-    }
-
-}
->>>>>>> Stashed changes
