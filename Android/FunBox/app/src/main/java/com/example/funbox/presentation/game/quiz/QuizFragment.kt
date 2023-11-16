bcd8300ce71fb4307490711180e609e1c4874227
littlesam95 <littlesam95@naver.com>
2023-11-17 01:31:59 +0900

    [feat] 게임 화면 View, ViewModel 뼈대 구성
    게임 화면 View, ViewModel 뼈대 구성

---
+++ Android/FunBox/app/src/main/java/com/example/funbox/presentation/game/quiz/QuizFragment.kt
@@ -0,0 +1,81 @@
+package com.example.funbox.presentation.game.quiz
+
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.UiThread
+import androidx.fragment.app.viewModels
+import com.example.funbox.R
+import com.example.funbox.databinding.FragmentQuizBinding
+import com.example.funbox.presentation.BaseFragment
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+
+class QuizFragment : BaseFragment<FragmentQuizBinding>(R.layout.fragment_quiz), OnMapReadyCallback {
+
+    private val viewModel: QuizViewModel by viewModels()
+    private lateinit var mapView: MapView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+        mapView = binding.mapViewGame
+        mapView.onCreate(savedInstanceState)
+
+        val cfm = childFragmentManager
+        val mapFragment = cfm.findFragmentById(R.id.map_view_game) as MapFragment?
+            ?: MapFragment.newInstance().also { mapFragment ->
+                cfm.beginTransaction().add(R.id.map_view_game, mapFragment).commit()
+            }
+        mapFragment.getMapAsync(this)
+
+        collectLatestFlow(viewModel.quizUiEvent) { handleUiEvent(it) }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    @UiThread
+    override fun onMapReady(p0: NaverMap) {
+
+    }
+
+    private fun handleUiEvent(event: QuizUiEvent) = when (event) {
+        is QuizUiEvent.NetworkErrorEvent -> {
+            showSnackBar(R.string.network_error_message)
+        }
+    }
+}
