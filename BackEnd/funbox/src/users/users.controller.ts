e046e346eee118b192b3fea5bb3f61f1710b4314
k33ps <k33ps66@gmail.com>
2023-11-16 20:10:46 +0900

    [fix] TypeORM Entity 수정
    DB설계의 의도에 맞게 TypeORM의 Entity의 설정을 개선하였다.
    
    [feat] post 요청 api 구현
    post /users의 api를 구현하였다.
    body로 오는 {locX,locY}를 (현재는 user[1]에) 갱신하고,
    그 주변의 유저(현재는 모든 유저)의 DTO를 반환한다.

--- BackEnd/funbox/src/users/users.controller.ts
+++ BackEnd/funbox/src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { UserLocationDto } from './dto/user-location.dto';
+import { NearUsersDto } from './dto/near-users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +16,11 @@ export class UsersController {
     @Get('/create')
     createUser(): Promise<User> {
         return this.usersService.createUsertest();
+    }
+
+    @Post()
+    async updateUserLocationAndReturnNearUsers(@Body() userLocationDto: UserLocationDto): Promise<NearUsersDto[]> {
+        await this.usersService.updateUserLocation(userLocationDto);
+        return await this.usersService.findNearUsers(userLocationDto);
     }
 }
--- BackEnd/funbox/src/users/users.service.ts
+++ BackEnd/funbox/src/users/users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from './user.entity';
+import { UserLocationDto } from './dto/user-location.dto';
+import { NearUsersDto } from './dto/near-users.dto';
 
 @Injectable()
 export class UsersService {
@@ -13,5 +15,36 @@ export class UsersService {
         user.message =  "hihi";
         user.messaged_at = "123123";
         return await user.save();
+    }
+
+    async updateUserLocation(userLocationDto: UserLocationDto): Promise<User> {
+        const user = await User.findOne({where : {id:1}}) // [!]OAuth 구현 후, id:1 부분 변경 필요
+        user.locX = userLocationDto.locX;
+        user.locY = userLocationDto.locY;
+        await user.save();
+        return user;
+    }
+
+    async findNearUsers(userLocationDto: UserLocationDto) : Promise<NearUsersDto[]> {
+        const users = await findNearUsersAlgorithm() // [!] 주변 조회 알고리즘 고도화 필요
+        const nearUsersDto = [];
+        users.forEach(user => { 
+            nearUsersDto.push(transFromUserToDto(user));
+        });
+        return nearUsersDto;
+
+        function transFromUserToDto(user: User) { //[?] 구조분해할당으로 간지나게 하고싶음...
+            const dto = new NearUsersDto();
+            dto.id = user.id;
+            dto.username = user.username;
+            dto.locX = user.locX;
+            dto.locY = user.locY;
+            dto.isMsgInAnHour = true; // [!] timestamp 관련 로직 필요
+            return dto;
+        }
+
+        async function findNearUsersAlgorithm(): Promise<User[]> {
+            return await User.find()
+        }
     }
 }
