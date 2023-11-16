4baf9d7940a9879468a54e2ca1b235cce3f73c32
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2023-11-16 22:35:18 +0900

    [feat] 단일 유저 조회 API 구현
    유저 엔티티 객체로 정해진 형식의 응답을 만들기 위해 UserResponseDto를 사용했다.
    
    Closes: #63

--- BackEnd/funbox/src/users/users.controller.ts
+++ BackEnd/funbox/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { UserResponseDto } from './dto/user-response.dto';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +15,11 @@ export class UsersController {
     @Get('/create')
     createUser(): Promise<User> {
         return this.usersService.createUsertest();
+    }
+
+    @Get('/:id')
+    async getUserById(@Param('id') id: number): Promise<UserResponseDto> {
+        const user = await this.usersService.getUserById(id)
+        return UserResponseDto.of(user);
     }
 }
--- BackEnd/funbox/src/users/users.service.ts
+++ BackEnd/funbox/src/users/users.service.ts
@@ -14,4 +14,12 @@ export class UsersService {
         user.messaged_at = "123123";
         return await user.save();
     }
+
+    async getUserById(id: number): Promise<User> {
+        const found = await User.findOneBy({id: id});
+        if (!found) {
+            throw new NotFoundException(`Can't find User with id ${id}`);
+        }
+        return found;
+    }
 }
