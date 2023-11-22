b7bade17f610a75af6a72f6e606ea1dfec00f0dd
littlesam95 <littlesam95@naver.com>
2023-11-22 19:17:40 +0900

    [style] 답안 체크 Dialog에 onClick 속성 부여
    1. 우선은 통신 종료 Dialog가 호출되도록 함
    
    Close: #78

--- Android/FunBox/app/src/main/java/com/example/funbox/presentation/game/quiz/AnswerCheckFragment.kt
+++ Android/FunBox/app/src/main/java/com/example/funbox/presentation/game/quiz/AnswerCheckFragment.kt
@@ -1,17 +1,27 @@
 package com.example.funbox.presentation.game.quiz
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.funbox.R
 import com.example.funbox.databinding.FragmentAnswerCheckBinding
 import com.example.funbox.presentation.BaseDialogFragment
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class AnswerCheckFragment : BaseDialogFragment<FragmentAnswerCheckBinding>(R.layout.fragment_answer_check) {
 
     private val viewModel: QuizViewModel by activityViewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
 
+        lifecycleScope.launch {
+            viewModel.quizUiEvent.collectLatest {
+                if (it == QuizUiEvent.QuizAnswerCheck) dismiss()
+            }
+        }
+    }
 }
