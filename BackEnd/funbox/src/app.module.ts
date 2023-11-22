84eda65e2d1c03fc17beac9ceb2d22919ef24a74
k33ps <k33ps66@gmail.com>
2023-11-22 22:02:44 +0900

    [feature] auth 모듈/컨트롤러/서비스 생성
    nest g module auth
    nest g controller auth --no-spec
    nest g service auth --no-spec

--- BackEnd/funbox/src/app.module.ts
+++ BackEnd/funbox/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
