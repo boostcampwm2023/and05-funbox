f06f9311e1a1b349135af92906836063af6727d6
k33ps <k33ps66@gmail.com>
2023-11-22 23:50:32 +0900

    [feature] idOAuth hash화 구현
    * 보안을 위해 idOAuth를 crypto 모듈을 사용해 hash화
      - 알고리즘 : sha256

--- BackEnd/funbox/src/auth/auth.service.ts
+++ BackEnd/funbox/src/auth/auth.service.ts
@@ -1,12 +1,30 @@
 import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { User } from 'src/users/user.entity';
 import { UserAuthDto } from './dto/user-auth.dto';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class AuthService {
-    async createNullUser(id_oauth: string): Promise<UserAuthDto> {
+    async createNullUser(idOauth: string): Promise<UserAuthDto> {
         const user = new User();
+        this.setUserNull(user, idOauth);
+        try {
+            await user.save();
+        } catch (error) {
+            
+            if(error.code === 'ER_DUP_ENTRY') {
+                throw new ConflictException("Existing id (OAuth)");
+            } else {
+                console.log(error);
+                throw new InternalServerErrorException();
+            }
+        }
+        return UserAuthDto.of(user);
+    }
+
+    setUserNull(user: User, idOauth: string): User{
         const now = new Date();
+        
         user.username = null;
         user.created_at = now;
         user.profile_url = null;
@@ -15,17 +33,13 @@ export class AuthService {
         user.message = null;
         user.messaged_at = null;
         user.type_login = "NAVER";
-        user.id_oauth = id_oauth;
-        try {
-            await user.save();
-        } catch (error) {
-            if(error.code === 'ER_DUP_ENTRY') {
-                throw new ConflictException("Existing id (OAuth)");
-            } else {
-                throw new InternalServerErrorException();
-            }
-        }
-        
-        return UserAuthDto.of(user);
+        user.id_oauth = this.hashedId(idOauth);
+
+        return user;
+    }
+
+    hashedId(idOauth: string): string {
+        const salt = "teamrpg";
+        return crypto.createHash('sha256').update(idOauth + salt).digest('hex');
     }
 }
