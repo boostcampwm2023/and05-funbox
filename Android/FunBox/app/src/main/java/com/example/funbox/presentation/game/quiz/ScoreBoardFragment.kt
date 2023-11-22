6071f37e11ed3fafe80b63792e49ca0920168147
littlesam95 <littlesam95@naver.com>
2023-11-22 19:18:15 +0900

    [style] 점수판 Dialog에 onClick 속성 부여
    1. 메인 화면(MapFragment)로 이동하도록 함.
    
    Close: #78

--- Android/FunBox/app/src/main/java/com/example/funbox/presentation/game/quiz/ScoreBoardFragment.kt
+++ Android/FunBox/app/src/main/java/com/example/funbox/presentation/game/quiz/ScoreBoardFragment.kt
@@ -1,13 +1,27 @@
 package com.example.funbox.presentation.game.quiz
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.funbox.R
 import com.example.funbox.databinding.FragmentScoreBoardBinding
 import com.example.funbox.presentation.BaseDialogFragment
-
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class ScoreBoardFragment : BaseDialogFragment<FragmentScoreBoardBinding>(R.layout.fragment_score_board) {
 
     private val viewModel: QuizViewModel by activityViewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+
+        lifecycleScope.launch {
+            viewModel.quizUiEvent.collectLatest {
+                if (it == QuizUiEvent.QuizScoreBoard) dismiss()
+            }
+        }
+    }
 }
