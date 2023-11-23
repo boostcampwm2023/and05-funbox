8d20df8fd0ab67d1b465fb38c7137ac11eb59e97
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2023-11-23 13:05:17 +0900

    [refactor] Prettier 전체 적용
    이미 커밋한 파일들에도 포맷팅을 적용하기 위해, npm run format 명령으로 Prettier를 전체 적용했습니다.
    
    Closes: #99

--- BackEnd/funbox/src/users/users.service.ts
+++ BackEnd/funbox/src/users/users.service.ts
@@ -5,61 +5,65 @@ import { NearUsersDto } from './dto/near-users.dto';
 
 @Injectable()
 export class UsersService {
-    async createUsertest(): Promise<User> {
-        const user = new User();
-        user.username = "test";
-        user.created_at = new Date();
-        user.profile_url = "132123";
-        user.locX = 123;
-        user.locY = 123;
-        user.message =  "hihi";
-        user.messaged_at = new Date();
-        return await user.save();
+  async createUsertest(): Promise<User> {
+    const user = new User();
+    user.username = 'test';
+    user.created_at = new Date();
+    user.profile_url = '132123';
+    user.locX = 123;
+    user.locY = 123;
+    user.message = 'hihi';
+    user.messaged_at = new Date();
+    return await user.save();
+  }
+
+  async updateUserLocation(userLocationDto: UserLocationDto): Promise<User> {
+    const user = await User.findOne({ where: { id: 1 } }); // [!]OAuth 구현 후, id:1 부분 변경 필요
+    user.locX = userLocationDto.locX;
+    user.locY = userLocationDto.locY;
+    await user.save();
+    return user;
+  }
+
+  async findNearUsers(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    userLocationDto: UserLocationDto,
+  ): Promise<NearUsersDto[]> {
+    const users = await findNearUsersAlgorithm(); // [!] 주변 조회 알고리즘 고도화 필요
+    const nearUsersDto = [];
+    users.forEach((user) => {
+      nearUsersDto.push(transFromUserToDto(user));
+    });
+    return nearUsersDto;
+
+    function transFromUserToDto(user: User) {
+      //[?] 구조분해할당으로 간지나게 하고싶음...
+      const dto = new NearUsersDto();
+      dto.id = user.id;
+      dto.username = user.username;
+      dto.locX = user.locX;
+      dto.locY = user.locY;
+      dto.isMsgInAnHour = true; // [!] timestamp 관련 로직 필요
+      return dto;
     }
 
-    async updateUserLocation(userLocationDto: UserLocationDto): Promise<User> {
-        const user = await User.findOne({where : {id:1}}) // [!]OAuth 구현 후, id:1 부분 변경 필요
-        user.locX = userLocationDto.locX;
-        user.locY = userLocationDto.locY;
-        await user.save();
-        return user;
+    async function findNearUsersAlgorithm(): Promise<User[]> {
+      return await User.find();
     }
+  }
 
-    async findNearUsers(userLocationDto: UserLocationDto) : Promise<NearUsersDto[]> {
-        const users = await findNearUsersAlgorithm() // [!] 주변 조회 알고리즘 고도화 필요
-        const nearUsersDto = [];
-        users.forEach(user => { 
-            nearUsersDto.push(transFromUserToDto(user));
-        });
-        return nearUsersDto;
-
-        function transFromUserToDto(user: User) { //[?] 구조분해할당으로 간지나게 하고싶음...
-            const dto = new NearUsersDto();
-            dto.id = user.id;
-            dto.username = user.username;
-            dto.locX = user.locX;
-            dto.locY = user.locY;
-            dto.isMsgInAnHour = true; // [!] timestamp 관련 로직 필요
-            return dto;
-        }
-
-        async function findNearUsersAlgorithm(): Promise<User[]> {
-            return await User.find()
-        }
+  async getUserById(id: number): Promise<User> {
+    const found = await User.findOneBy({ id: id });
+    if (!found) {
+      throw new NotFoundException(`Can't find User with id ${id}`);
     }
-  
-    async getUserById(id: number): Promise<User> {
-        const found = await User.findOneBy({id: id});
-        if (!found) {
-            throw new NotFoundException(`Can't find User with id ${id}`);
-        }
-        return found;
-    }
+    return found;
+  }
 
-    async updateUserMessage(id: number, message: string): Promise<User> {
-        const user = await this.getUserById(id);
-        user.message = message;
-        user.messaged_at = new Date();
-        return await user.save();
-    }
+  async updateUserMessage(id: number, message: string): Promise<User> {
+    const user = await this.getUserById(id);
+    user.message = message;
+    user.messaged_at = new Date();
+    return await user.save();
+  }
 }
