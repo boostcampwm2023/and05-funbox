1a2e6d47e503bb67846ec1114f79ad1f26896793
littlesam95 <littlesam95@naver.com>
2023-11-27 16:20:27 +0900

    [style] 퀴즈 게임 화면 UI 변경
    1. 현재 클라이언트가 문제를 내는지, 답을 맞추는지 그 상태를 열거형 클래스로 정의하고, UiState에 필드로 선언한 후 상태에 따라 다른 CardView가 출력되도록 하였다.
    
    Close: #110

--- Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/game/quiz/QuizBindingAdapters.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/game/quiz/QuizBindingAdapters.kt
@@ -1,3 +1,31 @@
 package com.rpg.funbox.presentation.game.quiz
 
+import android.view.View
 import androidx.databinding.BindingAdapter
+import com.google.android.material.card.MaterialCardView
+
+@BindingAdapter("app:answer_card_visible")
+fun MaterialCardView.bindAnswerVisibility(userQuizState: QuizUiState) {
+    visibility = when (userQuizState.isUserQuizState) {
+        true -> {
+            View.GONE
+        }
+
+        false -> {
+            View.VISIBLE
+        }
+    }
+}
+
+@BindingAdapter("app:quiz_card_visible")
+fun MaterialCardView.bindQuizVisibility(userQuizState: QuizUiState) {
+    visibility = when (userQuizState.isUserQuizState) {
+        true -> {
+            View.VISIBLE
+        }
+
+        false -> {
+            View.GONE
+        }
+    }
+}
