fafb0b6c5882a904e4573b91f33eacebc8ef046f
littlesam95 <littlesam95@naver.com>
2023-11-28 14:34:45 +0900

    [refactor] QuizFragment에서 Data Binding 문법 수정
    1. BindingAdapter를 사용하지 않고 Data Binding 문법을 사용하여 CardView의 visibility를 클라이언트의 상태에 따라 변경될 수 있도록 하였음

--- Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/game/quiz/QuizUiState.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/game/quiz/QuizUiState.kt
@@ -2,7 +2,7 @@ package com.rpg.funbox.presentation.game.quiz
 
 data class QuizUiState(
     val answerValidState: Boolean = false,
-    val userQuizState: UserQuizState = UserQuizState.Quiz
+    val userQuizState: UserQuizState = UserQuizState.Answer
 ) {
     val isAnswerSubmitBtnEnable: Boolean = (answerValidState)
     val isUserQuizState: Boolean = (userQuizState == UserQuizState.Quiz)
