069aacd8f74e2bf3a7c7e66775876259a9097c06
littlesam95 <littlesam95@naver.com>
2023-11-29 21:04:44 +0900

    [feat] Jwt Decoder 오브젝트 수정
    1. JsonObject를 Json 데이터로 바꾸는 방법을 생각하지 못 해서, Payload에 존재하는 element는 id, username, iat, exp만 존재하므로 JsonObject의 get() 메소드로 얻은 4가지 element를 인자로 가지는 데이터 클래스 객체를 반환하도록 하였다.
    
    Close: #41

--- Android/FunBox/app/src/main/java/com/rpg/funbox/data/JwtDecoder.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/data/JwtDecoder.kt
@@ -1,27 +1,38 @@
 package com.rpg.funbox.data
 
 import android.util.Base64
-import com.google.gson.Gson
 import com.google.gson.JsonParser
 import com.rpg.funbox.data.dto.UserAuthDto
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.nio.charset.StandardCharsets
 
 object JwtDecoder {
 
-    private val gson = Gson()
-    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    //private val gson = Gson()
+    //private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
-    fun getUser(accessToken: String): UserAuthDto? {
+    fun getUser(accessToken: String): UserAuthDto {
         val payloadJwt = accessToken.split(".")[1]
         val decodedPayload =
             String(Base64.decode(payloadJwt, Base64.DEFAULT), StandardCharsets.UTF_8)
         val jsonObject = JsonParser.parseString(decodedPayload).asJsonObject
-        val json = gson.toJson(jsonObject, JwtDecoder::class.java)
 
-        val moshiAdapter: JsonAdapter<UserAuthDto> = moshi.adapter(UserAuthDto::class.java)
-        return moshiAdapter.fromJson(json)
+        var userName: String? = null
+        if (!jsonObject.get("username").isJsonNull) {
+            userName = jsonObject.get("username").asString
+        }
+
+        return UserAuthDto(
+            jsonObject.get("id").asInt,
+            userName,
+            jsonObject.get("iat").asInt,
+            jsonObject.get("exp").asInt
+        )
+
+        /*
+            val json = gson.toJson(jsonObject, JwtDecoder::class.java)
+
+            val moshiAdapter: JsonAdapter<UserAuthDto> = moshi.adapter(UserAuthDto::class.java)
+            return moshiAdapter.fromJsonValue(decodedPayload)
+        */
     }
 }
