044665545f9fdc53f2ffb6d38ceab2aa966e86c1
thsdud135 <kytbible@gmail.com>
2023-11-30 06:36:09 +0900

    [refactor] DialogFragment 적용

--- Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/map/MessageDialog.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/map/MessageDialog.kt
@@ -6,23 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
+import com.rpg.funbox.R
 import com.rpg.funbox.databinding.DialogMessageBinding
+import com.rpg.funbox.presentation.BaseDialogFragment
 
-class MessageDialog: DialogFragment() {
-
-    private var _binding: DialogMessageBinding? = null
-    private val binding get() = _binding!!
+class MessageDialog: BaseDialogFragment<DialogMessageBinding>(R.layout.dialog_message) {
 
     private val viewModel: MapViewModel by activityViewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = DialogMessageBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,13 +20,5 @@ class MessageDialog: DialogFragment() {
             dismiss()
         }
     }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-
 
 }
