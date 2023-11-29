e54c7133d77b2bbc6a4d7396d27150b53230ffde
littlesam95 <littlesam95@naver.com>
2023-11-29 21:13:39 +0900

    [feat] Naver OAuth Access Token을 발급받는 로직 수정
    1. 이전에는 OAuthLoginCallback 객체를 따로 Singleton으로 선언했었는데, Access Token을 발급받기 전에 Access Token을 서버에 전송하는 통신이 이루어졌다.
    2. 그래서 Access Token이 서버에 전송되지 않아서 그냥 OAuthLoginCallback을 TitleFragment 내부에 선언하였다.
    3. 그리고 회원 정보는 서버에서 받기 때문에, ProfileCallback과 관련된 코드는 제거하였다.
    
    Close: #41

--- Android/FunBox/app/src/main/java/com/rpg/funbox/data/dto/UserAuthDto.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/data/dto/UserAuthDto.kt
@@ -1,8 +1,17 @@
 package com.rpg.funbox.data.dto
 
+import com.squareup.moshi.Json
+
 data class UserAuthDto(
-    val iat: Int,
-    val exp: Int,
+    @field:Json(name = "id")
     val id: Int,
-    val username: String?
+
+    @field:Json(name = "username")
+    val userName: String?,
+
+    @field:Json(name = "iat")
+    val iat: Int,
+
+    @field:Json(name = "exp")
+    val exp: Int
 )
