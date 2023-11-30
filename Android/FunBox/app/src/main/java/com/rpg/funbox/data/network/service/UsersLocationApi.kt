c71bf7581dd91a9369507752ccc49f934421cb1d
littlesam95 <littlesam95@naver.com>
2023-11-30 23:27:16 +0900

    [refactor] 필요 없는 코드 삭제
    1. 위치 정보 POST 통신 후 수신하는 response의 body가 List의 형태이기 때문에 Object가 아닌 Array의 형태로 파싱되도록 수정

--- Android/FunBox/app/src/main/java/com/rpg/funbox/data/network/service/UsersLocationApi.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/data/network/service/UsersLocationApi.kt
@@ -2,7 +2,6 @@ package com.rpg.funbox.data.network.service
 
 import com.rpg.funbox.data.dto.UserLocation
 import com.rpg.funbox.data.dto.UsersLocationRequest
-import com.rpg.funbox.data.dto.UsersLocationResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
