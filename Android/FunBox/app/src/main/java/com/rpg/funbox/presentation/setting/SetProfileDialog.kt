a05d649da0bfa2e1790bc626ff45e3ab0651ab34
littlesam95 <littlesam95@naver.com>
2023-12-03 15:31:24 +0900

    [feat] 환경설정에서 프로필 사진 변경 기능 구현
    1. ProfileFragment에서와 마찬가지로 프로필 사진을 변경하는 기능을 그대로 SetProfileDialog에 추가하였습니다.
    2. 근데 관련 권한 설정을 하는 코드를 작성하지 않아서 이 부분은 나중에 추가하겠습니다.

--- Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/setting/SetProfileDialog.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/setting/SetProfileDialog.kt
@@ -1,27 +1,79 @@
 package com.rpg.funbox.presentation.setting
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.rpg.funbox.R
-import com.rpg.funbox.databinding.DialogPositiveBinding
 import com.rpg.funbox.databinding.DialogProfileChangeBinding
 import com.rpg.funbox.presentation.BaseDialogFragment
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
+@SuppressLint("IntentReset")
 class SetProfileDialog : BaseDialogFragment<DialogProfileChangeBinding>(R.layout.dialog_profile_change) {
+
     private val viewModel: SettingViewModel by activityViewModels()
+    private var launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val imagePath = result.data?.data
+                val file = File(absolutelyPath(imagePath))
+                val requestFile = file.asRequestBody("image/*".toMediaTypeOrNull())
+                val body = MultipartBody.Part.createFormData("file", file.name, requestFile)
+
+                viewModel.selectNewProfile(imagePath, body)
+            }
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
 
         lifecycleScope.launch {
-            viewModel.settingUiEvent.collectLatest {
-                if (it == SettingUiEvent.CloseName) dismiss()
+            viewModel.settingUiEvent.collectLatest { uiEvent ->
+                when (uiEvent) {
+                    is SettingUiEvent.SelectProfile -> {
+                        getProfileImage()
+                    }
+
+                    is SettingUiEvent.CloseSetProfileDialog -> {
+                        dismiss()
+                    }
+
+                    else -> {}
+                }
             }
         }
+    }
+
+    private fun getProfileImage() {
+        val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        chooserIntent.putExtra(Intent.EXTRA_INTENT, intent)
+        chooserIntent.putExtra(Intent.EXTRA_TITLE, resources.getString(R.string.msg_select_picture))
+        launcher.launch(chooserIntent)
+    }
+
+    private fun absolutelyPath(uri: Uri?): String {
+        val proj: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor: Cursor? =
+            uri?.let { requireActivity().contentResolver.query(it, proj, null, null, null) }
+        val index = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor?.moveToFirst()
+
+        return cursor!!.getString(index!!)
     }
 }
