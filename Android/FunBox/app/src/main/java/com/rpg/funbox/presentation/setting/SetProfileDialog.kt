39281c0add72f46feb97202e268d29146e9f815a
littlesam95 <littlesam95@naver.com>
2023-12-04 21:39:31 +0900

    [fix] 사진 고르면 팅기는 오류 수정
    1. 사진을 가져올 수 있는 권한을 추가하였음

--- Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/setting/SetProfileDialog.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/setting/SetProfileDialog.kt
@@ -1,19 +1,19 @@
 package com.rpg.funbox.presentation.setting
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.rpg.funbox.R
 import com.rpg.funbox.databinding.DialogProfileChangeBinding
 import com.rpg.funbox.presentation.BaseDialogFragment
+import com.rpg.funbox.presentation.login.AccessPermission
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -25,15 +25,25 @@ import java.io.File
 class SetProfileDialog : BaseDialogFragment<DialogProfileChangeBinding>(R.layout.dialog_profile_change) {
 
     private val viewModel: SettingViewModel by activityViewModels()
-    private var launcher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val imagePath = result.data?.data
-                val file = File(absolutelyPath(imagePath))
+    private val profileImagePicker =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            val path = absolutelyPath(uri)
+            if (path != null) {
+                val file = File(path)
                 val requestFile = file.asRequestBody("image/*".toMediaTypeOrNull())
                 val body = MultipartBody.Part.createFormData("file", file.name, requestFile)
 
-                viewModel.selectNewProfile(imagePath, body)
+                viewModel.selectNewProfile(uri, body)
+            }
+        }
+    private val requestMultiPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            var flag = true
+            it.entries.forEach { entry ->
+                flag = entry.value
+            }
+            if (flag) {
+                profileImagePicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
             }
         }
 
@@ -45,7 +55,7 @@ class SetProfileDialog : BaseDialogFragment<DialogProfileChangeBinding>(R.layout
             viewModel.settingUiEvent.collectLatest { uiEvent ->
                 when (uiEvent) {
                     is SettingUiEvent.SelectProfile -> {
-                        getProfileImage()
+                        requestMultiPermissions.launch(AccessPermission.profilePermissionList)
                     }
 
                     is SettingUiEvent.CloseSetProfileDialog -> {
@@ -58,22 +68,13 @@ class SetProfileDialog : BaseDialogFragment<DialogProfileChangeBinding>(R.layout
         }
     }
 
-    private fun getProfileImage() {
-        val chooserIntent = Intent(Intent.ACTION_CHOOSER)
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        intent.type = "image/*"
-        chooserIntent.putExtra(Intent.EXTRA_INTENT, intent)
-        chooserIntent.putExtra(Intent.EXTRA_TITLE, resources.getString(R.string.msg_select_picture))
-        launcher.launch(chooserIntent)
-    }
-
-    private fun absolutelyPath(uri: Uri?): String {
+    private fun absolutelyPath(uri: Uri?): String? {
         val proj: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
         val cursor: Cursor? =
             uri?.let { requireActivity().contentResolver.query(it, proj, null, null, null) }
         val index = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
         cursor?.moveToFirst()
 
-        return cursor!!.getString(index!!)
+        return index?.let { cursor.getString(it) }
     }
 }
