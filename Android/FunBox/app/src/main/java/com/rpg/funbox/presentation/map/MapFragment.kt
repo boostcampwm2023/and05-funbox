31f8eee96a9d49598e53e838cf5a4547f823c517
littlesam95 <littlesam95@naver.com>
2023-12-13 12:12:09 +0900

    [fix] 위치 권한 허용 거부 시 환경 설정 화면 이동 후 팅기는 현상 해결

--- Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/map/MapFragment.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/map/MapFragment.kt
@@ -40,24 +40,16 @@ import com.rpg.funbox.presentation.fadeInOut
 import com.rpg.funbox.presentation.login.AccessPermission
 import com.rpg.funbox.presentation.login.AccessPermission.LOCATION_PERMISSION_REQUEST_CODE
 import com.rpg.funbox.presentation.slideLeft
-
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.yield
 import timber.log.Timber
 import java.util.Timer
 import kotlin.concurrent.scheduleAtFixedRate
-import kotlin.coroutines.coroutineContext
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnMapReadyCallback {
 
