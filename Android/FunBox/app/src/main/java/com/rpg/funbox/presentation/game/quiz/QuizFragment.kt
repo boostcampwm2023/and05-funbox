2ff1fdc98f6128ba31054f87123a99b386ff8a2b
thsdud135 <kytbible@gmail.com>
2023-12-13 12:19:35 +0900

    [fix] 위치 권한오류(퀴즈)

--- Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/game/quiz/QuizFragment.kt
+++ Android/FunBox/app/src/main/java/com/rpg/funbox/presentation/game/quiz/QuizFragment.kt
@@ -82,6 +82,7 @@ class QuizFragment : BaseFragment<FragmentQuizBinding>(R.layout.fragment_quiz), 
         super.onCreate(savedInstanceState)
 
         fusedLocationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        requestMultiPermissions.launch(AccessPermission.locationPermissionList)
         if (!requireActivity().checkPermission(AccessPermission.locationPermissionList)) {
             requestMultiPermissions.launch(AccessPermission.locationPermissionList)
         }
